e1c1a522fd439ad058abef18f742e429d5758376
Agustin <agustinsurila@gmail.com>
2023-04-03 17:05:45 -0300

    Axios agregado

--- src/components/shared/LabelPicker.tsx
+++ src/components/shared/LabelPicker.tsx
@@ -1,23 +1,38 @@
 import { useQuery } from '@tanstack/react-query';
+import { githubApi } from '../../api/githubApi';
 
-const getLabels = async (): Promise<any> => {
-  const res = await fetch('https://api.github.com/repos/facebook/react/labels');
-  const data = await res.json();
+interface Label {
+  color: string;
+  default: boolean;
+  description?: string;
+  id: number;
+  name: string;
+  node_id: string;
+  url: string;
+}
+
+const getLabels = async (): Promise<Label[]> => {
+  const { data } = await githubApi.get<Label[]>('/labels');
 
   return data;
 };
 
 export const LabelPicker = () => {
-  const labelsQuery = useQuery(['labels'], getLabels);
+  const labelsQuery = useQuery(['labels'], getLabels, {
+    refetchOnWindowFocus: false,
+  });
 
   return (
     <div>
-      <span
-        className='badge rounded-pill m-1 label-picker'
-        style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
-      >
-        Primary
-      </span>
+      {labelsQuery.data?.map((l) => (
+        <span
+          className='badge rounded-pill m-1 label-picker'
+          style={{ border: `1px solid #${l.color}`, color: `#${l.color}` }}
+          key={l.id}
+        >
+          {l.name}
+        </span>
+      ))}
     </div>
   );
 };
