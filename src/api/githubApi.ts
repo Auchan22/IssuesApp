1dfcbc5c8bd0f588cd455584870409265057c11b
Agustin <agustinsurila@gmail.com>
2023-04-11 23:41:58 -0300

    Incluyendo Github API Token

--- src/api/githubApi.ts
+++ src/api/githubApi.ts
@@ -2,5 +2,8 @@ import axios from 'axios';
 
 export const githubApi = axios.create({
   baseURL: 'https://api.github.com/repos/facebook/react',
-  headers: {},
+  headers: {
+    Authorization:
+      'Bearer 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',
+  },
 });
