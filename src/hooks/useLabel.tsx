78b3a1ef46bab2b38bd5e841fdc69fe964d573d8
Agustin <agustinsurila@gmail.com>
2023-04-11 23:16:03 -0300

    InitialDate y PlaceHolderData 🤔

--- src/hooks/useLabel.tsx
+++ src/hooks/useLabel.tsx
@@ -14,7 +14,26 @@ const getLabels = async (): Promise<Label[]> => {
 export const useLabel = () => {
   const labelsQuery = useQuery(['labels'], getLabels, {
     refetchOnWindowFocus: false,
-    staleTime: 1000 * 60 * 60,
+    // staleTime: 1000 * 60 * 60,
+    placeholderData: [
+      {
+        id: 717031390,
+        node_id: 'MDU6TGFiZWw3MTcwMzEzOTA=',
+        url: 'https://api.github.com/repos/facebook/react/labels/good%20first%20issue',
+        name: 'good first issue',
+        color: '6ce26a',
+        default: true,
+      },
+      {
+        id: 1757816973,
+        node_id: 'MDU6TGFiZWwxNzU3ODE2OTcz',
+        url: 'https://api.github.com/repos/facebook/react/labels/dependencies',
+        name: 'dependencies',
+        color: '0366d6',
+        default: false,
+        description: 'Pull requests that update a dependency file',
+      },
+    ],
   });
   return { labelsQuery };
 };
