47667c4a95d3653ad8688c6c04da457e98340131
Agustin <agustinsurila@gmail.com>
2023-04-11 23:27:25 -0300

    Seleccionando Labels

--- src/components/shared/LabelPicker.tsx
+++ src/components/shared/LabelPicker.tsx
@@ -1,7 +1,12 @@
 import { useLabel } from '../../hooks/useLabel';
 import { LoadingSpinner } from './LoadingSpinner';
 
-export const LabelPicker = () => {
+interface Props {
+  selectedLabels: string[];
+  onChange: (labelName: string) => void;
+}
+
+export const LabelPicker: React.FC<Props> = ({ selectedLabels, onChange }) => {
   const { labelsQuery } = useLabel();
 
   if (labelsQuery.isLoading) return <LoadingSpinner />;
@@ -10,9 +15,12 @@ export const LabelPicker = () => {
     <div>
       {labelsQuery.data?.map((l) => (
         <span
-          className='badge rounded-pill m-1 label-picker'
+          className={`badge rounded-pill m-1 label-picker ${
+            selectedLabels.includes(l.name) ? 'label-active' : ''
+          }`}
           style={{ border: `1px solid #${l.color}`, color: `#${l.color}` }}
           key={l.id}
+          onClick={() => onChange(l.name)}
         >
           {l.name}
         </span>
--- src/components/views/ListView.tsx
+++ src/components/views/ListView.tsx
@@ -1,7 +1,16 @@
+import { useState } from 'react';
 import { IssueList } from '../shared/IssueList';
 import { LabelPicker } from '../shared/LabelPicker';
 
 export const ListView = () => {
+  const [selectedLabels, setSelectedLabels] = useState<string[]>([]);
+
+  const onLabelChange = (l: string) => {
+    selectedLabels.includes(l)
+      ? setSelectedLabels(selectedLabels.filter((s) => s !== l))
+      : setSelectedLabels([...selectedLabels, l]);
+  };
+
   return (
     <div className='row mt-5'>
       <div className='col-8'>
@@ -9,7 +18,10 @@ export const ListView = () => {
       </div>
 
       <div className='col-4'>
-        <LabelPicker />
+        <LabelPicker
+          selectedLabels={selectedLabels}
+          onChange={(labelName: string) => onLabelChange(labelName)}
+        />
       </div>
     </div>
   );
